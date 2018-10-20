ea6817e063a08b72c0ed4b1d881e0fa69ef4b76c
Dobes Vandermeer <dobesv@gmail.com>
2018-10-20 11:22:22 -0700

    Improve error reporting in JSONPackager

--- JSONPackager.js
+++ JSONPackager.js
@@ -5,9 +5,18 @@ class JSONPackager extends Packager {
         super(...args);
         this.combinedAst = {};
     }
+
     addAsset(asset) {
-        Object.assign(this.combinedAst, JSON.parse(asset.generated.json));
+        if(!asset.generated.json)
+            throw new Error(`JSON missing in parsed asset: ${asset.relativeName}; got ${Object.keys(asset.generated).join(',')}`);
+        try {
+            Object.assign(this.combinedAst, JSON.parse(asset.generated.json));
+        } catch (e) {
+            e.message = `In ${asset.name}: ${e.message}:\n${asset.generated.json}`;
+            throw e;
+        }
     }
+
     async end() {
         await this.write(JSON.stringify(this.combinedAst, null, 2));
         return super.end();
